d2a9185f0fe2afc93591d359a87ceda780294e4e
yhlyl <yh_android@163.com>
2017-09-12 23:57:57 +0800

    chan goroutine

---
+++ studyGo/gobookcode/chan.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+var c chan string
+
+func Pinpang()  {
+	i := 0
+	for{
+		fmt.Println(<-c)
+		c <- fmt.Sprintf("From Pinpang: Hi,#%d",i)
+		i++
+	}
+}
+
+func main() {
+	c = make(chan string)
+	go Pinpang()
+
+	for i:=0;i<10;i++ {
+		c <- fmt.Sprintf("From Main: Hello,#%d",i)
+		fmt.Println(<-c)
+	}
+}
