97d47c338f14d9dab0851f4c11b7fa65a5ab0c78
游浩 <yh_android@163.com>
2020-02-25 22:00:30 +0800

    [添加]--Fibonacci

--- helloworlddonc/conc.go
+++ helloworlddonc/conc.go
@@ -1,3 +1,12 @@
+/*
+ * @Author: yhlyl
+ * @Date: 2018-09-07 00:13:34
+ * @LastEditTime: 2020-02-25 21:58:25
+ * @LastEditors: yhlyl
+ * @Description:
+ * @FilePath: /yhgolang/helloworlddonc/conc.go
+ * @https://github.com/android-coco
+ */
 package main
 
 import (
@@ -5,6 +14,11 @@ import (
 )
 
 func main() {
+	f1 := f()
+	fmt.Println(f1())
+	fmt.Println(f1())
+	fmt.Println(f1())
+	return
 	ch := make(chan string)
 	for i := 0; i < 5000; i++ {
 		go printHellWrold(i, ch)
@@ -15,11 +29,18 @@ func main() {
 		fmt.Println(msg)
 	}
 
-
 }
 
 func printHellWrold(i int, ch chan string) {
-	for{
+	for {
 		ch <- fmt.Sprintf("Hello world %d!\n", i)
+	}
+}
+
+func f() func() int {
+	a, b := 0, 1
+	return func() int {
+		a, b = b, a+b
+		return a
 	}
 }
